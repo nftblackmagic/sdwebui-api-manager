3b9310438e1a7ad56e176a0f6adc3b2d2202ce28
marcio lemos <contato.marciovrl@gmail.com>
2020-01-11 13:52:49 -0300

    implementation of endpoits tests /user, /question and /alternatives

--- test.py
+++ test.py
@@ -5,6 +5,30 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message": "Fast API in Python"}
+    assert response.json() == {'message': 'Fast API in Python'}
+
+
+def test_read_user():
+    response = client.get('/user')
+    assert response.status_code == 200
+    assert len(response.json()) != 0
+
+
+def test_read_question():
+    response = client.get('/question/1')
+    assert response.status_code == 200
+    assert response.json()['position'] == 1
+
+
+def test_read_question_invalid():
+    response = client.get('/question/0')
+    assert response.status_code == 400
+    assert response.json() == {'message': 'Error'}
+
+
+def test_read_alternatives():
+    response = client.get('/alternatives/1')
+    assert response.status_code == 200
+    assert response.json()[1]['question_id'] == 1
