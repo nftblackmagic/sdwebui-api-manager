f955fdc0ebde276240f060dfa5bb310be4f77455
marcio lemos <contato.marciovrl@gmail.com>
2020-01-12 10:11:17 -0300

    implementation of endpoits /answer and /result

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from starlette.responses import Response
+from pydantic import BaseModel
+from typing import List
 
 import json
 
 app = FastAPI()
+
+
+class Answer(BaseModel):
+    question_id: int
+    alternative_id: int
+
+
+class UserAnswer(BaseModel):
+    user_id: int
+    answers: List[Answer]
 
 
 @app.get("/")
@@ -45,3 +57,56 @@ def read_alternatives(question_id: int):
             alternatives_question.append(alternative)
 
     return alternatives_question
+
+
+@app.post("/answer")
+def create_answer(userAnswer: UserAnswer):
+    userAnswer = userAnswer.dict()
+    answers = []
+    result = []
+
+    with open('data/alternatives.json') as stream:
+        alternatives = json.load(stream)
+
+    for question in userAnswer['answers']:
+        for alternative in alternatives:
+            if alternative['question_id'] == question['question_id']:
+                answers.append(alternative['alternative'])
+                break
+
+    with open('data/cars.json') as stream:
+        cars = json.load(stream)
+
+    for car in cars:
+        if answers[0] in car.values() and answers[1] in car.values() and answers[2] in car.values():
+            result.append(car)
+
+    return result
+
+
+@app.get("/result/{user_id}")
+def read_result(user_id: int):
+    user_result = []
+
+    with open('data/result.json') as stream:
+        results = json.load(stream)
+
+    with open('data/users.json') as stream:
+        users = json.load(stream)
+
+    with open('data/cars.json') as stream:
+        cars = json.load(stream)
+
+    for result in results:
+        if result['user_id'] == user_id:
+            for user in users:
+                if user['id'] == result['user_id']:
+                    user_result.append({'user': user})
+                    break
+
+        for car_id in result['cars']:
+            for car in cars:
+                if car_id == car['id']:
+                    user_result.append(car)
+
+    return user_result
