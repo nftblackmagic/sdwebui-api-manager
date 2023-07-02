19da7c47d8a9b71f92d3f27181b789572914a95a
An <anzhanghust@gmail.com>
2023-07-01 18:04:41 -0700

    fix launch script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
+python3 -m uvicorn app.main:app --reload --host 0.0.0.0 --port 5001
+
+cd ./stable-diffusion-webui
 bash <(wget -qO- https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh) -f --listen --xformers --enable-insecure-extension-access --theme dark --api 
 
-python3 -m uvicorn app.main:app --reload --host 0.0.0.0 --port 5001
